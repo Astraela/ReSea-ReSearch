2cc9979b3a786759e4c16ba49af295e5c4b9682b
CreatorOfFame <40199794+CreatorOfFame@users.noreply.github.com>
2022-03-28 16:09:03 +0200

    changed choicemaker text & fixed making choice bug

--- ReSea ReSearch/Assets/Scripts/Choice.cs
+++ ReSea ReSearch/Assets/Scripts/Choice.cs
@@ -3,8 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using Yarn;
 
-public class Choice : Singleton<Choice>
+public class Choice : MonoBehaviour
 {
     public string choice;
+
+    public static Choice Instance;
+
+    void Start(){
+        if(Instance == null){
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
 }
 
